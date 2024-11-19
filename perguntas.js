6a901373160665567cfd79f2843496999800e050
bizorinho <149188978+bizorinho@users.noreply.github.com>
2024-11-19 11:13:19 -0300

    cu

--- perguntas.js
+++ perguntas.js
@@ -27,29 +27,4 @@ criaCartao(
     'Mendel, ao estudar ervilhas em cruzamentos diíbridos, notou que a herança de uma característica não influenciava a outra. Assim, ele concluiu que os alelos de diferentes genes se segregam independentemente.'
 )
 
-criaCartao(
-    'Genética',
-    'Considere uma planta de ervilha que é heterozigota para duas características: cor da semente (amarela é dominante sobre verde) e forma da semente (lisa é dominante sobre rugosa). Qual será a proporção fenotípica esperada na geração F2?',
-    '9:3:3:1 (9 amarelas lisas : 3 amarelas rugosas : 3 verdes lisas : 1 verde rugosa).'
-)
-
-
-criaCartao(
-    'Genética',
-    'Oque é Co-odominância?',
-    'Co-dominância é um padrão de herança onde ambos os alelos de um par são expressos igualmente no fenótipo. Um exemplo é o tipo sanguíneo AB, que combina características dos alelos A e B.'
-)
-
-criaCartao(
-    'Genética',
-    'Oque é Heredogramas?',
-    'Heredogramas são representações gráficas que mostram a herança de características genéticas em famílias ao longo de gerações, permitindo identificar padrões de herança e a presença de doenças genéticas.'
-)
-
-criaCartao(
-    'Genética',
-    'Qual é a diferença entre Genótipo e fenótipo?',
-    'Genótipo é a composição genética de um organismo, representando a combinação de alelos para um gene. Fenótipo, por outro lado, são as características observáveis resultantes da interação entre o genótipo e o ambiente, como cor, forma e comportamento.'
-)
-
 
